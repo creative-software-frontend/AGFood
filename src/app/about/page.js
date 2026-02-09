0a3c07269eb304d5189e438cb8a9c05123de71cd
samiacodes <emiliyalamia@gmail.com>
2026-02-09 17:58:38 +0600

    About page all route set

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,12 +1,34 @@
-import AboutContent from "@/components/sections/about/AboutContent";
 import AboutHero from "@/components/sections/about/AboutHero";
-
+import CompanyInfo from "@/components/sections/about/CompanyInfo";
+import VisionMission from "@/components/sections/about/VisionMission";
+import Chairman from "@/components/sections/about/Chairman";
+import Board from "@/components/sections/about/Board";
+import Management from "@/components/sections/about/Management";
 
 export default function AboutPage() {
   return (
     <main>
       <AboutHero />
-      <AboutContent />
+
+      <section id="info" className="scroll-mt-24">
+        <CompanyInfo />
+      </section>
+
+      <section id="vision" className="scroll-mt-24">
+        <VisionMission />
+      </section>
+
+      <section id="chairman" className="scroll-mt-24">
+        <Chairman />
+      </section>
+
+      <section id="board" className="scroll-mt-24">
+        <Board />
+      </section>
+
+      <section id="management" className="scroll-mt-24">
+        <Management />
+      </section>
     </main>
   );
 }
