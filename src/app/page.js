818912d045baa688c95783b2b858d41b54b1622d
samiacodes <emiliyalamia@gmail.com>
2026-02-09 12:14:40 +0600

    Banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Hero from "@/components/sections/Hero";
 
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>hello world</h1>
+      <Hero />
     </div>
   );
 }
