b123d0b841a80f271febbd9c2bef4f5b02ddc90b
samiacodes <emiliyalamia@gmail.com>
2026-02-09 16:48:04 +0600

    Reviews Section & AboutCollapseSection.tsx

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import About from "@/components/sections/About";
+import AboutCollapseSection from "@/components/sections/AboutCollapseSection";
 import Hero from "@/components/sections/Hero";
+import Reviews from "@/components/sections/Reviews";
 
 
 export default function Home() {
@@ -7,6 +9,8 @@ export default function Home() {
     <div className="min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
         <Hero></Hero>
         <About></About>
+        <AboutCollapseSection></AboutCollapseSection>
+        <Reviews></Reviews>
     </div>
   );
 }
